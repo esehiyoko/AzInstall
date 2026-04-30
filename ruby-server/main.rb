ad605c9c6138054a9835dbd719cd04609ddb4b32
Daisuke Shimotoku <shimotoku@ds.itc.u-tokyo.ac.jp>
2026-05-01 02:59:54 +0900

    feat: add LISTEN_PORT configuration for Sinatra and update binding logic

--- ruby-server/main.rb
+++ ruby-server/main.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 require 'fileutils'
 require 'open3'
+require 'dotenv'
+Dotenv.load(File.expand_path('.env', __dir__))
 
 PID_FILE = File.expand_path('pids.txt', __dir__)
 
@@ -16,12 +18,13 @@ end
 def start_sinatra
   sinatra_dir = File.expand_path('sinatra', __dir__)
   config_ru   = File.join(sinatra_dir, 'config.ru')
+  port = ENV['LISTEN_PORT'] || '4567'
   crt = File.expand_path('server.crt', __dir__)
   key = File.expand_path('server.key', __dir__)
   bind = if File.exist?(crt) && File.exist?(key)
-    "ssl://0.0.0.0:4567?cert=#{crt}&key=#{key}"
+    "ssl://0.0.0.0:#{port}?cert=#{crt}&key=#{key}"
   else
-    'tcp://0.0.0.0:4567'
+    "tcp://0.0.0.0:#{port}"
   end
   pid = Process.spawn('bundle', 'exec', 'puma', '-b', bind, config_ru, chdir: sinatra_dir, out: '/dev/null', err: '/dev/null')
   File.open(PID_FILE, 'a') { |f| f.puts pid }
